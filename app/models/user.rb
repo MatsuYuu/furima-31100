1a79d42d4203f189ce630f0a2cd20011648c7205
MatsuYuu <matsu_doit@icloud.com>
2020-11-06 15:57:07 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
   validates :nickname,        presence: true
   validates :birth_date,      presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :last_name   
-    validates :first_name      
-    validates :last_name_kana  
-    validates :first_name_kana 
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX 
-
-         
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { 'yama' }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太郎' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_date            { '1950-04-01' }
+  end
+end
