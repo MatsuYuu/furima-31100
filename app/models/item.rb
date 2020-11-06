5ee347f01cb0368c8d3ea97d6358586903308e63
MatsuYuu <matsu_doit@icloud.com>
2020-11-06 19:59:40 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :day, :status, :area, :burden
+
+  with_options presence: true do
+    validates :category       
+    validates :day
+    validates :status       
+    validates :area  
+    validates :burden         
+  end
+
   has_one_attached :image
+
 end
