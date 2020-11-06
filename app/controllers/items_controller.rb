b2af98bc0ff474b44679b9f51580d689080cd97e
MatsuYuu <matsu_doit@icloud.com>
2020-11-06 21:37:32 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -9,6 +11,6 @@ class ItemsController < ApplicationController
   private
 
   def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :category, :day, :status, :area, :burden).merge(user_id: current_user.id)
   end
 end
