972f4e89e182c25929fa436360b877583ea31774
MatsuYuu <matsu_doit@icloud.com>
2020-11-08 00:28:24 +0900

    JSの計算

---
+++ app/javascript/channels/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const profitDom = document.getElementById("profit");
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  addTaxDom.innerHTML = Math.floor(inputValue / 10); 
+  profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+})
+
+})
