b2463b6bbf89c705519ec403c0f2f007f7cfff5d
MatsuYuu <matsu_doit@icloud.com>
2020-11-09 14:24:54 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,14 +19,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def show
-  end
-
   def update
-    @item.update(item_params)
+    if @item.update(item_params)
+       redirect_to root_path
+    else
+      render :edit
+    end
   end
 
 
