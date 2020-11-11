02d5400483cb7f89816e348567ec3b705deaa616
MatsuYuu <matsu_doit@icloud.com>
2020-11-11 22:01:51 +0900

    クライアントサイド実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -6,7 +6,6 @@ const pay = () => {
     
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
-
     const card = {
       number: formData.get("number"),
       cvc: formData.get("cvc"),
@@ -17,8 +16,16 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
