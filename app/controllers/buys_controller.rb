675914c907793022dab50cb07b0c39f82c6d44d3
MatsuYuu <matsu_doit@icloud.com>
2020-11-11 20:12:26 +0900

    情報遷移(name,price)

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,13 +1,16 @@
 class BuysController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @buy_item = BuyItem.new
   end
 
   def create
     @buy_item = BuyItem.new(buyitem_params)
+    @item = Item.find(params[:item_id])
     if  @buy_item.valid?
         @buy_item.save
+        @item.save
         redirect_to action: :index
     else
         render action: :index
